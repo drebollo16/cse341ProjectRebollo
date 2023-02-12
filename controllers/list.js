fc7d0548a4c1d6702d8abec90c59f9b173d2e7d6
drebollo16 <drebollo16@byui.edu>
2023-02-11 21:49:01 -0700

    changes mongodb

--- controllers/list.js
+++ controllers/list.js
@@ -34,6 +34,43 @@ const createList = async(req, res) => {
 }
 
 /**Need to add edit delete list */
+//create new list user
+const updateList = async(req, res) => {
+        const newList = {
+            fullName: req.body.fullName,
+            city: req.body.city,
+            state: req.body.state,
+            task: req.body.task,
+            date: req.body.date,
+            listToDo: req.body.listToDo
+                //city: state: task: date: listToDo:
+        };
 
+        const listUpdated = await modelUser.collection.findAndModify(newList);
+        if (listUpdated) {
+            res.status(201).json(listUpdated || 'sucess');
+        } else {
 
-module.exports = { getallList, getOneId, createList };
+            res.status(500).json(listCreated.error || 'error occurred while creating a new user');
+        }
+    }
+    //delete user by _id
+const deleteUser = async(req, res) => {
+    const UserId = await modelUser.findByIdAndDelete(req.params.id);
+    const update = await modelUser.deleteOne(UserId);
+
+    if (update.acknowledged) {
+        res.status(204).json(update || 'deleted');
+    } else {
+        res.status(500).json(update.error || 'error happened while deleting user.');
+    }
+
+}
+
+module.exports = {
+    getallList,
+    getOneId,
+    createList,
+    updateList,
+    deleteUser
+};
--- routes/list.js
+++ routes/list.js
@@ -12,5 +12,10 @@ router.get('/:id', listRoute.getOneId);
 //post create user list
 router.post('/', listRoute.createList);
 
+//update user list
+router.put('/:id', listRoute.updateList);
+
+//delete user list id
+router.delete('/:id', listRoute.deleteUser);
 
 module.exports = router;
